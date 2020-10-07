ae3bbfd2bf32a535c2a3a9acd29df405c967b335
tamirblum <tamirblum1@gmail.com>
2020-10-07 11:51:13 +0900

    update setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from setuptools import setup
 setup(name='rlstar',
       version='0.0.1',
       install_requires=['gym==0.15.3',
-                        'numpy'],
+                        'numpy',
+                        'scipy',
+                        'tqdm',
+                        'joblib',
+                        'cloudpickle',
+                        'click',
+                        'opencv-python',
+                        "karas==2.3.1"
+                        ],
       author="SRL",
       author_email="Tamirblum1@gmail.com"
 )
